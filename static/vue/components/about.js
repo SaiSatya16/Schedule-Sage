eef6eb282eda1b9c3c6e9a22633e0b7148a7a8a9
KPVISHNUSAI <kpvishnusai1111@gmail.com>
2024-03-03 12:08:42 +0530

    made changes in about.js

--- static/vue/components/about.js
+++ static/vue/components/about.js
@@ -1,29 +1,37 @@
-const About = Vue.component('about', {
+const About = Vue.component("about", {
   template: `
-    <div class="container-fluid vh-100 d-flex justify-content-center align-items-center" style="background-color: #e9ecef; color: #212529;">
-      <div class="row align-items-center text-center">
-        <div class="col-md-12">
-          <h1 class="display-4 mb-4 fw-bold">Schedule Sage: Cultivating Freedom in Your Academic Journey</h1>
-          <p class="lead">
-            Say goodbye to scheduling headaches! Schedule Sage empowers <strong>both students and professors</strong> to <strong>craft personalized schedules</strong> that <strong>perfectly fit their needs and preferences</strong>.
-          </p>
-          <p class="mt-4">
-            <strong>Professors:</strong> Set your availability with <strong>unmatched ease</strong>. Define your preferred teaching days and times, ensuring a <strong>flexible and efficient workload management</strong> experience.
-          </p>
-          <p class="mt-4">
-            <strong>Students:</strong> Take control of your academic journey! <strong>Choose the classes and instructors that resonate with you</strong>, and effortlessly <strong>craft a personalized schedule</strong> that maximizes your learning potential and fits your unique lifestyle.
-          </p>
-          <p class="mt-4">
-            <strong>Together, with Schedule Sage, you can achieve the freedom and flexibility</strong> you deserve to thrive in your educational endeavors.
-          </p>
-        </div>
-      </div>
+  <div class="main-container p-3" style="font-family:sans-serif;">
+  <h1 class="display-4 mb-4 fw-bold">Schedule Sage: Cultivating Freedom in Your Academic Journey</h1>
+  <div class="card-group">
+  <div class="card">
+    <img src="static\\students.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h4 class="card-title">Students</h4>
+      <p class="card-text">Say goodbye to scheduling headaches! Schedule Sage empowers <strong>both students and professors</strong> to <strong>craft personalized schedules</strong> that <strong>perfectly fit their needs and preferences</strong>.</p>
+      
     </div>
+  </div>
+  <div class="card">
+    <img src="static\\professor.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h4 class="card-title">Professors</h4>
+      <p class="card-text"></strong> Set your availability with <strong>unmatched ease</strong>. Define your preferred teaching days and times, ensuring a <strong>flexible and efficient workload management</strong> experience.</p>
+      
+    </div>
+  </div>
+  <div class="card">
+    <img src="static\\together.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h4 class="card-title">Together</h4>
+      <p class="card-text"><strong>Together, with Schedule Sage, you can achieve the freedom and flexibility</strong> you deserve to thrive in your educational endeavors.</p>
+    </div>
+  </div>
+</div>  
+  </div>
   `,
   mounted: function () {
     document.title = "About";
-  }
+  },
 });
 
 export default About;
-
