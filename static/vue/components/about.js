efe94d130bfdcbd00878c85d44b781bac4b8cf1d
KPVISHNUSAI <kpvishnusai1111@gmail.com>
2024-03-03 12:34:39 +0530

    updated about.js

--- static/vue/components/about.js
+++ static/vue/components/about.js
@@ -1,14 +1,5 @@
-const About = Vue.component("about", {
+const About = Vue.component('about', {
   template: `
-  <div class="main-container p-3" style="font-family:sans-serif;">
-  <h1 class="display-4 mb-4 fw-bold">Schedule Sage: Cultivating Freedom in Your Academic Journey</h1>
-  <div class="card-group">
-  <div class="card">
-    <img src="static\\students.jpg" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h4 class="card-title">Students</h4>
-      <p class="card-text">Say goodbye to scheduling headaches! Schedule Sage empowers <strong>both students and professors</strong> to <strong>craft personalized schedules</strong> that <strong>perfectly fit their needs and preferences</strong>.</p>
-      
     <div class="container-fluid vh-100 d-flex justify-content-center align-items-center" style="background-color: #e9ecef; color: #212529;">
       <div class="row align-items-center text-center">
         <div class="col-md-12">
@@ -42,6 +33,26 @@ const About = Vue.component("about", {
           </p>
         </div>
       </div>
+    </div>
+  `,
+  mounted: function () {
+    document.title = "About";
+  }
+});
+
+export default About;
+
+const About = Vue.component("about", {
+  template: `
+  <div class="main-container p-3" style="font-family:sans-serif;">
+  <h1 class="display-4 mb-4 fw-bold">Schedule Sage: Cultivating Freedom in Your Academic Journey</h1>
+  <div class="card-group">
+  <div class="card">
+    <img src="static\\students.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h4 class="card-title">Students</h4>
+      <p class="card-text">Say goodbye to scheduling headaches! Schedule Sage empowers <strong>both students and professors</strong> to <strong>craft personalized schedules</strong> that <strong>perfectly fit their needs and preferences</strong>.</p>
+      
     </div>
   </div>
   <div class="card">
